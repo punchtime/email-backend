460fa34ab47cb09fa601ad7062706a8ccb227475
Haroen Viaene <hello@haroen.me>
2016-04-22 14:18:25 +0200

    node.js

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 > Note: this is a work in progress, and not yet a deployed backend.
 
-The email backend is deployed on [uberspace](https://uberspace.de) and mostly hosts the addition of clients to employers. It's written in php :disappointed: because. Use your own Mailgun key :smile:, you fill this in in your own `config.json`, just like `config.example.json`
+The email backend is deployed on [uberspace](https://uberspace.de) and mostly hosts the addition of clients to employers. ~~It's written in php :disappointed: because~~ It's written in node.js. Use your own Mailgun key :smile:, you fill this in in your own `config.json`, just like `config.example.json`
+
+1. fill in `config.json`
+2. `npm install`
+3. `npm start`
+4. Have a drink :beer:
 
 ## related
 
--- index.js
+++ index.js
@@ -4,53 +4,72 @@ const mailgun = require('mailgun-js')({apiKey: config.key, domain: config.domain
 const Firebase = require('firebase');
 const base = new Firebase('https://scorching-inferno-1467.firebaseio.com/');
 let employer = 'k', code = 'x';
-let emailTemp = `
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <style>
-  body {
-    font-family: -apple-system, Roboto, sans-serif;
-  }
-  img {
-    width: 10em;
-  }
-  code {
-    font-family: Menlo, monospace;
-    background-color: lightgrey;
-    margin: 1em;
-    padding: .3em;
-  }
-  article {
-    margin: 2em 0;
-  }
-  </style>
-</head>
-<body>
-  <p>Hey</p>
-  <article>
-    <p>Welcome to <a href="https://punchtime.io">punchtime</a>. Your employer (${employer}) invited you to join his team.</p>
-    <p>First download the app:</p>
-    <p>
-      <a href="https://play.google.com/"><img alt="install punchtime from the app store" src="https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png" /></a>
-    </p>
-    <p>Then you can add yourself to the team in settings and entering</p>
-    <pre><code>${code}</code></pre>
-    <p>You can also click on this <a href="punchtime://${code}" title="${code}">link</a></p>
-  </article>
-  <p>Greetings!</p>
-  <p>Punchtime Team</p>
-  <p><a href="https://punchtime.io">punchtime.io</a></p>
-</body>
-</html>
-`
 
-mailgun.messages().send({
-  from: 'Punchtime <'+config.from+'@'+config.domain+'>',
-  to: 'hello@haroen.me',
-  subject: 'Hello',
-  text: 'Testing some Mailgun awesomness!'
-}, function (error, body) {
-  console.log(body);
+base.child('invites').on('child_added',snap=>{
+  console.log(snap.val());
+  if (snap.val().sent != true) {
+    let code = snap.key();
+    let employer = snap.val().company.name;
+    let emailTemp = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+      <style>
+      body {
+        font-family: -apple-system, Roboto, sans-serif;
+      }
+      img {
+        width: 10em;
+      }
+      code {
+        font-family: Menlo, monospace;
+        background-color: lightgrey;
+        margin: 1em;
+        padding: .3em;
+      }
+      article {
+        margin: 2em 0;
+      }
+      .muted {
+        font-size: .8em;
+        color: grey;
+        font-style: italic;
+        margin: 1em;
+        text-align: center;
+        border-top: 1px solid grey;
+      }
+      </style>
+    </head>
+    <body>
+      <p>Hey</p>
+      <article>
+        <p>Welcome to <a href="https://punchtime.io">punchtime</a>. Your employer (${employer}) invited you to join his team.</p>
+        <p>First download the app:</p>
+        <p>
+          <a href="https://play.google.com/"><img alt="install punchtime from the app store" src="https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png" /></a>
+        </p>
+        <p>Then you can add yourself to the team in settings and entering</p>
+        <pre><code>${code}</code></pre>
+        <p>You can also click on this <a href="punchtime://${code}" title="${code}">link</a></p>
+      </article>
+      <p>Greetings!</p>
+      <p>Punchtime Team</p>
+      <p><a href="https://punchtime.io">punchtime.io</a></p>
+      <p class="muted">If you didn't expect this email, you can safely ignore it.</p>
+    </body>
+    </html>
+    `;
+
+    mailgun.messages().send({
+      from: 'Punchtime Invitation<'+config.from+'@'+config.domain+'>',
+      to: snap.val().email,
+      subject: 'You were invited to punchtime!',
+      html: emailTemp
+    }, function (error, body) {
+      console.log(body);
+    });
+
+    base.child('invites').child(snap.key()).child('sent').set(true);
+  }
 });
